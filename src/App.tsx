78f27913ca81439b31848fc1e174320c3a022a93
aditya-sps <152249472+aditya-sps@users.noreply.github.com>
2024-03-04 14:37:01 +0530

    Added screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Login from "./screens/Login";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignUp from "./screens/Signup";
+import Dashboard from "./screens/Dashboard";
+import Search from "./screens/Search";
+import Messages from "./screens/Messages";
+import Profile from "./screens/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="search" element={<Search />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
